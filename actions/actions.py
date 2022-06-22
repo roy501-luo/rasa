37bae63965b37a804b764e76e649ac793eb55352
luojun <1303809182@qq.com>
2022-06-22 16:59:17 +0800

    form

--- actions/actions.py
+++ actions/actions.py
@@ -1,27 +1,37 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
+from typing import Any, Dict, List, Text
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.events import SlotSet
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.forms import FormAction
+
+from service.normalization import text_to_date
+from service.weather import get_text_weather_date
 
 
-# This is a simple example for a custom action which utters "Hello World!"
+class WeatherFormAction(Action):
+    def name(self) -> Text:
+        return "action_weather_form_submit"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+    def run(
+        self, dispatch: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]
+    ) -> List[Dict]:
+        city = tracker.get_slot("address")
+        date_text = tracker.get_slot("date-time")
+
+        date_object = text_to_date(date_text)
+
+        if not date_object:  # parse date_time failed
+            msg = "暂不支持查询 {} 的天气".format([city, date_text])
+            dispatch.utter_message(msg)
+        else:
+            dispatch.utter_message(templete="utter_working_on_it")
+            try:
+                weather_data = get_text_weather_date(city, date_object, date_text)
+            except Exception as e:
+                exec_msg = str(e)
+                dispatch.utter_message(exec_msg)
+            else:
+                dispatch.utter_message(weather_data)
+
+        return []
